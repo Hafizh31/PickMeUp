f8c9ebb0aa8e6d3578f5d9e27abf57a68bbe7e0d
Naufal Mas Adha <63807994+NaufalMasAdha@users.noreply.github.com>
2021-03-30 03:34:27 +0700

    Add files via upload

---
+++ Sewa.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of interface InputData here.
+ *
+ * @author (Adika, Hafizh, Naufal)
+ * @version (30 Maret 2021)
+ */
+public interface Sewa 
+
+{
+     public void ProsesData();
+}
