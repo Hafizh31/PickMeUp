9bbe7ce6923a362a4416baa7ec851f192f1ea41d
adikaksr <64773004+adikaksr@users.noreply.github.com>
2021-03-30 03:30:05 +0700

    Add files via upload

---
+++ becak.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Write a description of class DSLR here.
+ *
+ * @author (Adika, Hafizh, Naufal)
+ * @version (30 Maret 2021)
+ */
+public class becak implements Sewa
+{
+    public void JenisPaket() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nMau sewa paket yang mana? :\n1. Paket1(Dengan Supir)\n2. Paket2(Tanpa Supir) ");
+        int paket = input.nextInt();
+        System.out.println("Anda akan menyewa becak dengan Paket " + paket );
+        System.out.println("Terimakasih telah menggunakan jasa kami");
+    }
+
+    public void ProsesData() {
+        JenisPaket();
+        }
+    
+}
